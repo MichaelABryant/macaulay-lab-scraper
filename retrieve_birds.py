cc57ec57f963b9b525faf258d5d9e939114acead
Michael Bryant <58651380+michaelbryantds@users.noreply.github.com>
2021-10-18 19:56:00 -0700

    Update retrieve_birds.py

--- retrieve_birds.py
+++ retrieve_birds.py
@@ -1,7 +1,9 @@
 import scraper as bird
 import pandas as pd
 
+#begin scraping for 2000 bird image names/urls
 df = bird.get_birds(2000, 0)
 
+#zip and output as csv
 compression_opts = dict(method='zip', archive_name='bird_urls_2011.csv')  
 df.to_csv('bird_urls_2011.zip', index=False, compression=compression_opts)
